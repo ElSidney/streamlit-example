3464f1c4fb6f1bf8765f6c05ae0211ffee42e349
James Sidney <ElSidney@users.noreply.github.com>
2023-10-24 12:22:17 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,10 +8,11 @@ import requests
 
 # Make a request to the OKEx API to get the latest price of a token future
 def get_price(symbol):
-    url = f"GET /api/v5/public/funding-rate?instId=BTC-USD-SWAP"
+    url = f"GET /api/v5/public/funding-rate?instId={symbol}"
     response = requests.get(url)
     data = response.json()
     return data
 
+get_price(BTC-USD-SWAP)
 
 st.write(data)
