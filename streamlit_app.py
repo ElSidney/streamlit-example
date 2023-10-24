35dd57ac316a7f22ec8358dab331e506357b84e2
James Sidney <ElSidney@users.noreply.github.com>
2023-10-24 11:29:17 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,13 +14,14 @@ forums](https://discuss.streamlit.io).
 
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
+GET /api/v5/public/instruments?instType=SPOT
 
 import requests
 import streamlit as st
 
 # Make a request to the OKEx API to get the latest price of a token future
 def get_price(symbol):
-    url = f"https://www.okex.com/api/spot/v3/instruments/{symbol}/ticker"
+    url = f"https://www.okex.com/api/v5/instruments?instType=SPOT&instId={symbol}"
     response = requests.get(url)
     data = response.json()
     return data["last"]
