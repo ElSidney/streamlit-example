a54ef35f748f4afc65ff5073048889f5717006d9
James Sidney <ElSidney@users.noreply.github.com>
2023-10-24 13:07:06 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import math
 import pandas as pd
 import streamlit as st
 import requests
-
+st.set_page_config(page_title="My App", page_icon=":cat:", layout="centered", initial_sidebar_state="auto", menu_items=None)
 st.title("The title")
 st.sidebar.write("sidebar")
 
